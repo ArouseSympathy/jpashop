54060520e6f8a4aef667c6068f08ec2cc4711c93
ArouseSympathy <cleankid99@naver.com>
2020-08-17 14:16:32 +0900

    v3.1 : fetch join과 콜렉션 페이징

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,8 +7,16 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class JpashopApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(JpashopApplication.class, args);
-	}
+        SpringApplication.run(JpashopApplication.class, args);
+    }
+
+    /*@Bean
+    Hibernate5Module hibernate5Module() {
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+        hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5Module;
+    }*/
+
 }
--- src/main/java/jpabook/jpashop/repository/OrderSearch.java
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -7,12 +7,8 @@ import lombok.Setter;
 @Getter @Setter
 public class OrderSearch {
 
-
-
     private String memberName; //회원이름
     private OrderStatus orderStatus; // ORDER, CANCEL
-
-
 
 
 }
